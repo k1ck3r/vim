de49235ab53555e3483fe4158c7d44bf1ebedec1
Yavor Marinov <32503925+k1ck3r@users.noreply.github.com>
2022-05-12 13:01:46 +0300

    Update .fzf.bash

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,14 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */root/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}~/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}/root/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "~/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/root/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "~/.fzf/shell/key-bindings.bash"
+source "/root/.fzf/shell/key-bindings.bash"
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
